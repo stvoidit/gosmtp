1ddea28ca9562094793a5b1b3dd31992e35a50e1
StVoid <stvoid.it@yandex.ru>
2020-08-03 13:37:32 +0300

    add attach from reader

--- Messanger.go
+++ Messanger.go
@@ -2,18 +2,24 @@ package gosmtp
 
 import (
 	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/h2non/filetype"
 )
 
 // Message - тело сообщения
 type Message struct {
-	to, cc, bcc, attaches []string
-	text, subject, from   string
+	to, cc, bcc         []string
+	text, subject, from string
+	attachments         []*Attachment
 }
 
 // NewMessage - create new message
@@ -59,8 +65,82 @@ func (m *Message) SetText(body string) *Message {
 
 // AddAttaches - add files
 func (m *Message) AddAttaches(attchs ...string) *Message {
-	m.attaches = attchs
+	for _, filename := range attchs {
+		if a, err := attachFile(filename); err == nil {
+			m.attachments = append(m.attachments, a)
+		} else {
+			fmt.Println("AddAttaches ERROR:", err)
+		}
+	}
 	return m
+}
+
+// AttacheReader - add file from reader
+func (m *Message) AttacheReader(r io.Reader, filename string) error {
+	a, err := attachReader(r, filename)
+	if err != nil {
+		return err
+	}
+	m.attachments = append(m.attachments, a)
+	return nil
+}
+
+// NewMessage - simple create new message for send
+func (m *Message) buildMessageBody() []byte {
+	withAttachments := len(m.attachments) > 0
+	var headers = make(map[string]string)
+	headers["From"] = m.from
+	if m.to != nil {
+		headers["To"] = strings.Join(m.to, ";")
+	} else {
+		m.to = make([]string, 0, 0)
+	}
+	if m.cc != nil {
+		headers["Cc"] = strings.Join(m.cc, ";")
+	} else {
+		m.cc = make([]string, 0, 0)
+	}
+	if m.bcc != nil {
+		headers["Bcc"] = strings.Join(m.bcc, ";")
+	} else {
+		m.bcc = make([]string, 0, 0)
+	}
+	headers["Subject"] = m.subject
+	headers["MIME-Version"] = "1.0"
+	headers["Date"] = time.Now().Format(time.RFC1123Z)
+	var buf bytes.Buffer
+	var writer = multipart.NewWriter(&buf)
+	var boundary = writer.Boundary()
+
+	for k, v := range headers {
+		buf.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
+	}
+
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf(`Content-Type: multipart/mixed; boundary="%s"`, boundary))
+		buf.WriteString("\r\n\r\n")
+		buf.WriteString(fmt.Sprintf("--%s\r\n", boundary))
+	}
+	buf.WriteString("Content-Type: text/plain; charset=utf-8\r\n")
+	buf.WriteString("MIME-Version: 1.0\r\n")
+	buf.WriteString("\r\n" + m.text)
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("\r\n--%s", boundary))
+		for _, v := range m.attachments {
+			buf.WriteString(fmt.Sprintf("\r\nContent-Type: %s\r\n", v.MIME))
+			buf.WriteString("Content-Transfer-Encoding: base64\r\n")
+			buf.WriteString("MIME-Version: 1.0\r\n")
+			buf.WriteString(fmt.Sprintf(`Content-Disposition: attachment; filename="%s"`, v.Filename))
+			buf.WriteString("\r\n\r\n")
+
+			var b = make([]byte, base64.StdEncoding.EncodedLen(len(v.Data)))
+			base64.StdEncoding.Encode(b, v.Data)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\r\n\r\n--%s", boundary))
+		}
+		buf.WriteString("--")
+	}
+	return buf.Bytes()
 }
 
 // Attachment - simple file attachment structure
@@ -71,32 +151,48 @@ type Attachment struct {
 }
 
 // attachFiles - attached files in message, match MIME type
-func attachFiles(src []string) ([]Attachment, error) {
-	var attachments = make([]Attachment, len(src))
+func attachFiles(src ...string) ([]*Attachment, error) {
+	var attachments = make([]*Attachment, len(src))
 	for i, filename := range src {
 		file, err := os.Open(filename)
 		if err != nil {
-			return attachments, err
+			return nil, err
 		}
 		defer file.Close()
-		var buff bytes.Buffer
-		if _, err := buff.ReadFrom(file); err != nil {
-			return attachments, err
+		a, err := attachReader(file, filename)
+		if err != nil {
+			return nil, err
 		}
-		var mime string
-		// TODO: hardcode, bad decision, but i needed
-		if strings.HasSuffix(filename, ".xlsx") {
-			mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-		} else {
-			if contentType, err := filetype.Match(buff.Bytes()); err != nil || contentType.Extension == "unknown" {
-				mime = http.DetectContentType(buff.Bytes())
-			} else {
-				mime = contentType.MIME.Value
-			}
-		}
-		_, fileName := filepath.Split(filename)
-		a := Attachment{Filename: fileName, MIME: mime, Data: buff.Bytes()}
 		attachments[i] = a
 	}
 	return attachments, nil
+}
+
+func attachReader(r io.Reader, filename string) (*Attachment, error) {
+	var buff bytes.Buffer
+	if _, err := buff.ReadFrom(r); err != nil {
+		return nil, err
+	}
+	var mime string
+	if strings.HasSuffix(filename, ".xlsx") {
+		mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	} else {
+		if contentType, err := filetype.Match(buff.Bytes()); err != nil || contentType.Extension == "unknown" {
+			mime = http.DetectContentType(buff.Bytes())
+		} else {
+			mime = contentType.MIME.Value
+		}
+	}
+	_, fileName := filepath.Split(filename)
+	return &Attachment{Filename: fileName, MIME: mime, Data: buff.Bytes()}, nil
+}
+
+// ! deprecated
+func attachFile(filename string) (*Attachment, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return attachReader(file, filename)
 }
--- Sender.go
+++ Sender.go
@@ -1,14 +1,8 @@
 package gosmtp
 
 import (
-	"bytes"
-	"encoding/base64"
 	"fmt"
-	"log"
-	"mime/multipart"
 	"net/smtp"
-	"strings"
-	"time"
 )
 
 // Sender - smtp client structure
@@ -18,9 +12,9 @@ type Sender struct {
 	messages                           []*Message
 }
 
-// Quit - close client
-func (s *Sender) Quit() {
-	s.client.Quit()
+// Close - close client
+func (s *Sender) Close() {
+	s.client.Close()
 }
 
 // AddMessage - add to the distribution queue
@@ -32,101 +26,40 @@ func (s *Sender) AddMessage(msgs ...*Message) {
 }
 
 // Send - simple send message
-func (s *Sender) Send() error {
+func (s *Sender) Send() (ERR error) {
 	for _, message := range s.messages {
 		s.messages = s.messages[1:] // resetting from the sent message list
 		if err := s.client.Mail(message.from); err != nil {
-			return err
+			ERR = fmt.Errorf("%s %q", err, message.from)
 		}
 		for _, recepient := range message.to {
 			if err := s.client.Rcpt(recepient); err != nil {
-				return err
+				ERR = fmt.Errorf("%s %q", err, recepient)
 			}
 		}
 		for _, copies := range message.cc {
 			if err := s.client.Rcpt(copies); err != nil {
-				return err
+				ERR = fmt.Errorf("%s %q", err, copies)
 			}
 		}
 		for _, secrets := range message.bcc {
 			if err := s.client.Rcpt(secrets); err != nil {
-				return err
+				ERR = fmt.Errorf("%s %q", err, secrets)
 			}
 		}
 		w, err := s.client.Data()
 		if err != nil {
+			s.client.Reset()
 			return err
 		}
 		if _, err := w.Write(message.buildMessageBody()); err != nil {
+			s.client.Reset()
 			return err
 		}
-		err = w.Close()
-		if err != nil {
+		if err := w.Close(); err != nil {
+			s.client.Reset()
 			return err
 		}
 	}
-	return nil
-}
-
-// NewMessage - simple create new message for send
-func (m *Message) buildMessageBody() []byte {
-	attachments, err := attachFiles(m.attaches)
-	if err != nil {
-		log.Panic(err)
-	}
-	withAttachments := len(attachments) > 0
-	var headers = make(map[string]string)
-	headers["From"] = m.from
-	if m.to != nil {
-		headers["To"] = strings.Join(m.to, ";")
-	} else {
-		m.to = make([]string, 0, 0)
-	}
-	if m.cc != nil {
-		headers["Cc"] = strings.Join(m.cc, ";")
-	} else {
-		m.cc = make([]string, 0, 0)
-	}
-	if m.bcc != nil {
-		headers["Bcc"] = strings.Join(m.bcc, ";")
-	} else {
-		m.bcc = make([]string, 0, 0)
-	}
-	headers["Subject"] = m.subject
-	headers["MIME-Version"] = "1.0"
-	headers["Date"] = time.Now().Format(time.RFC1123Z)
-	var buf = bytes.NewBuffer(nil)
-	var writer = multipart.NewWriter(buf)
-	defer writer.Close()
-	var boundary = writer.Boundary()
-
-	for k, v := range headers {
-		buf.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
-	}
-
-	if withAttachments {
-		buf.WriteString(fmt.Sprintf(`Content-Type: multipart/mixed; boundary="%s"`, boundary))
-		buf.WriteString("\r\n\r\n")
-		buf.WriteString(fmt.Sprintf("--%s\r\n", boundary))
-	}
-	buf.WriteString("Content-Type: text/plain; charset=utf-8\r\n")
-	buf.WriteString("MIME-Version: 1.0\r\n")
-	buf.WriteString("\r\n" + m.text)
-	if withAttachments {
-		buf.WriteString(fmt.Sprintf("\r\n--%s", boundary))
-		for _, v := range attachments {
-			buf.WriteString(fmt.Sprintf("\r\nContent-Type: %s\r\n", v.MIME))
-			buf.WriteString("Content-Transfer-Encoding: base64\r\n")
-			buf.WriteString("MIME-Version: 1.0\r\n")
-			buf.WriteString(fmt.Sprintf(`Content-Disposition: attachment; filename="%s"`, v.Filename))
-			buf.WriteString("\r\n\r\n")
-
-			var b = make([]byte, base64.StdEncoding.EncodedLen(len(v.Data)))
-			base64.StdEncoding.Encode(b, v.Data)
-			buf.Write(b)
-			buf.WriteString(fmt.Sprintf("\r\n\r\n--%s", boundary))
-		}
-		buf.WriteString("--")
-	}
-	return buf.Bytes()
+	return
 }
